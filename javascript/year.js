d8936a41bfff2996e800c929d39345f7f33eac65
pavel-buran <pavel.buran@seznam.cz>
2026-06-12 17:01:51 +0200

    Commit-001

---
+++ javascript/year.js
@@ -0,0 +1,35 @@
+  function validateYear() {
+    var yearInput = document.getElementById("comment_year");
+    var submitButton = document.getElementById("submit_button");
+    
+    // Zjistí aktuální rok (2026)
+    var currentYear = new Date().getFullYear().toString();
+
+    // Kontrola hodnoty
+    if (yearInput.value.trim() === currentYear) {
+      submitButton.removeAttribute("disabled");
+    } else {
+      submitButton.setAttribute("disabled", "disabled");
+    }
+  }
+  
+  // Pojistka pro jistotu: Spustí se hned po načtení skriptu a tlačítko zamkne
+  document.getElementById("submit_button").setAttribute("disabled", "disabled");
+
+  function validateYear2() {
+    var yearInput = document.getElementById("comment_year2");
+    var submitButton = document.getElementById("submit_button2");
+    
+    // Zjistí aktuální rok (2026)
+    var currentYear = new Date().getFullYear().toString();
+
+    // Kontrola hodnoty
+    if (yearInput.value.trim() === currentYear) {
+      submitButton.removeAttribute("disabled");
+    } else {
+      submitButton.setAttribute("disabled", "disabled");
+    }
+  }
+  
+  // Pojistka pro jistotu: Spustí se hned po načtení skriptu a tlačítko zamkne
+  document.getElementById("submit_button2").setAttribute("disabled", "disabled");
